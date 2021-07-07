b1be49643b291aba011516bbcf64992bc01a6e80
encls <51222153+encls@users.noreply.github.com>
2021-07-06 23:29:50 -0700

    Modified structure definitions

--- SystemPolicyInfo/defs.h
+++ SystemPolicyInfo/defs.h
@@ -1,61 +1,8 @@
 #pragma once
 
-static const UCHAR DecryptFnArgs[ 128 ] = {
-    0xc9, 0x98, 0xe5, 0x1b, 0xa3, 0xa9, 0x63, 0x2e, 0x56, 0xe1, 0xe2, 0x53, 0xe0, 0x65, 0x77, 0x7c,
-    0x3e, 0x26, 0x3d, 0x34, 0x5f, 0xb9, 0x87, 0xce, 0x86, 0xa9, 0xe7, 0xf2, 0x98, 0x08, 0x83, 0x14,
-    0x85, 0x1e, 0x83, 0x91, 0x9d, 0xbd, 0x3c, 0xc3, 0x22, 0x0c, 0x21, 0xbe, 0x4a, 0x78, 0x05, 0xb2,
-    0xce, 0x2d, 0x0e, 0x0b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-static const UCHAR DecryptFnIndex[ 32 ] = {
-    0x1d, 0x0e, 0x0f, 0x09, 0x1b, 0x01, 0x1a, 0x18, 0x1e, 0x05, 0x0b, 0x19, 0x02, 0x1f, 0x1f, 0x1f,
-    0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f
-};
-
-typedef struct _UNICODE_STRING
-{
-    USHORT Length;                                                          //0x0
-    USHORT MaximumLength;                                                   //0x2
-    WCHAR* Buffer;                                                          //0x8
-} UNICODE_STRING;
-
-//
-// 48 bytes
-// struct manages all three policy blocks in kernel
-// initial data is copied from POLICY_BLOCK header
-// block data is allocated on paged pool (SLS software licensing server tag) and holds POLICY_BLOCK info
-//
-typedef struct _SLS_DATA
-{
-    //
-    // BlockA size > 0x8
-    //
-    ULONG EncryptedDataSize;
-    ULONG ReservedA;
-    PSLS_ENCRYPTED_HEADER EncryptedData;
-
-    //
-    // BlockB size == 0xA0 (160)
-    //
-    ULONG EncyptDecryptArgSize;
-    ULONG ReservedB;
-    PSLS_ENCRYPT_DECRYPT_ARGS EncryptDecryptArgs;
-
-    //
-    // BlockC size == 0x8
-    //
-    ULONG KeySize;
-    ULONG ReservedC;
-    PSLS_KEY EncryptDecryptKey;
-} SLS_DATA;
-
 //
 // 4 bytes (ulong)
-// other methods are undocumented 
+// methods are mostly undocumented or have no symbol names
 //
 typedef enum _SYSTEM_POLICY_TYPE
 {
@@ -67,9 +14,9 @@ typedef enum _SYSTEM_POLICY_TYPE
     FileIntegrityUpdate,
     FileIntegrityQuery,
     NotImplemented = 102,
-    IsAppLicense = 109,
+    IsAppLicensed = 109,
     ClepKdf = 113,
-    UpdateOsPfnInRegistry = 204, // undocumented?
+    UpdateOsPFNInRegistry = 204, 
     GetAppPolicyValue = 208
 } SYSTEM_POLICY_TYPE;
 
@@ -119,10 +66,34 @@ typedef struct _SLS_KEY
 } SLS_KEY, * PSLS_KEY;
 
 //
-// Decrypted data
-// Size specified in decrypted header
+// SPCallServerHandleIsAppLicensed
 //
-typedef struct _SLS_DECRYPTED_DATA
+typedef struct _SLS_APP_LICENSED_BODY
+{
+    ULONG UnknownSizeA;
+    ULONGLONG UnknownA; // de d0 a6 da a5 10 00 00
+
+    ULONG UnknownSizeB;
+    ULONG UnknownB; // 3
+
+    ULONG MaxStringSize;
+    WCHAR* AppName;
+
+    ULONG UnknownSizeC; 
+    USHORT UnknownC; // 0
+
+    ULONG UnknownSizeD; // 0x1c
+    UCHAR Unknown[ 0x1C ]; // 01 05 00 00 00 00 00 05 15 00 00 00 79 da-c9 84 23 e2 f9 82 2c 25 c0 58 e8 03 00 00
+
+    ULONG UnknownSizeE;
+    ULONG UnknownE; // 4
+} SLS_APP_LICENSED_BODY;
+
+//
+// Decrypted header
+// Size specified in decrypted data
+//
+typedef struct _SLS_DECRYPTED_HEADER
 {
     ULONG PolicyTypeSize;
     SYSTEM_POLICY_TYPE PolicyType;
@@ -133,11 +104,8 @@ typedef struct _SLS_DECRYPTED_DATA
     ULONG KeySize;
     SLS_KEY EncryptKey;
 
-    ULONG PolicySize;
-    UNICODE_STRING PolicyName;
-
-
-} SLS_DECRYPTED_DATA;
+    SLS_APP_LICENSED_BODY Body;
+} SLS_DECRYPTED_HEADER;
 
 #define SLS_DATA_SIZE 296
 
@@ -145,38 +113,91 @@ typedef struct _SLS_DECRYPTED_DATA
 // > 8 bytes
 // encrypted data appended with xor checksum of decrypted data
 //
-typedef struct _SLS_ENCRYPTED_DATA
+typedef struct _SLS_ENCRYPTED_HEADER
 {
-    UCHAR EncryptedData[ SLS_DATA_SIZE ];
+    UCHAR EncryptedBody[ SLS_DATA_SIZE ];
     ULONGLONG XorChkKey;
-} SLS_ENCRYPTED_DATA;
+} SLS_ENCRYPTED_HEADER;
 
 //
 // encrypted input size - 8 bytes (removing XorChkKey?)
 // decrypted header data from block B decryption routine
 //
-typedef struct _SLS_DECRYPTED_HEADER
+typedef struct _SLS_DECRYPTED_DATA
 {
-    ULONG ArgCount;
+    ULONG ParameterCount;
+    ULONG DecryptedSize;
 
-    ULONG DecryptedDataSize;
-    SLS_DECRYPTED_DATA Data;
+    SLS_DECRYPTED_HEADER Data;
 
-    UCHAR Unk[6];
-} SLS_DECRYPTED_HEADER;
+    ULONG a;
+    USHORT b;
+} SLS_DECRYPTED_DATA;
 
 //
 // input/output structure for system call
 // block sizes + 12 must equal input length
 //
-typedef struct _SLS_ENCRYPTED_HEADER
+typedef struct _SLS_ENCRYPTED_DATA
 {
     ULONG EncryptedDataSize;
-    SLS_ENCRYPTED_DATA EncryptedData;
+    SLS_ENCRYPTED_HEADER EncryptedHeaderData;
 
     ULONG DecryptArgSize;
     SLS_ENCRYPT_DECRYPT_ARGS DecryptArgs;
 
     ULONG KeySize;
     SLS_KEY DecryptKey;
-} SLS_ENCRYPTED_HEADER, * PSLS_ENCRYPTED_HEADER;
+} SLS_ENCRYPTED_DATA, * PSLS_ENCRYPTED_DATA;
+
+//            Data
+// +-------------------------+
+// |                         |
+// |         Header          |
+// +-------------------------+
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |          Body           |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// |                         |
+// +-------------------------+
+//
+
+//
+// 48 bytes
+// struct manages all three policy blocks in kernel
+// initial data is copied from POLICY_BLOCK header
+// block data is allocated on paged pool (SLS software licensing server tag) and holds POLICY_BLOCK info
+//
+typedef struct _SP_DATA
+{
+    //
+    // BlockA size > 0x8
+    //
+    ULONG EncryptedDataSize;
+    ULONG ReservedA;
+    PSLS_ENCRYPTED_DATA EncryptedData;
+
+    //
+    // BlockB size == 0xA0 (160)
+    //
+    ULONG EncyptDecryptArgSize;
+    ULONG ReservedB;
+    PSLS_ENCRYPT_DECRYPT_ARGS EncryptDecryptArgs;
+
+    //
+    // BlockC size == 0x8
+    //
+    ULONG KeySize;
+    ULONG ReservedC;
+    PSLS_KEY EncryptDecryptKey;
+} SP_DATA;
